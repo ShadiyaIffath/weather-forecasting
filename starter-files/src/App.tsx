cf2e51f9a1a518c22fc17162e2070d8652b139b2
Shadiya Iffath <shadiya.iffath98@gmail.com>
2023-10-12 16:54:37 +0530

    weather api search completed

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,7 +1,23 @@
-const App = () => {
+import Search from './components/Search'
+import useForecast from './hooks/useForecast'
+
+const App = (): JSX.Element => {
+  const { place, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast()
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      {forecast != null ? (
+        <p>We have a forecast!</p>
+      ) : (
+        <Search
+          term={place}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        ></Search>
+      )}
     </main>
   )
 }
